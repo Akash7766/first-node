26b0cb3bf0ab022c37cd5f1c74384062e64e7880
akash7766 <mraakash01932@gmail.com>
2022-04-28 17:20:18 +0600

    db post successful

--- index.js
+++ index.js
@@ -4,12 +4,46 @@ const cors = require("cors");
 const port = 5000;
 
 app.use(cors());
+
+// dbUser2
+// sGuOO1Bc0lYgje5M
+
 app.use(express.json());
 const users = [
   { id: 1, name: "Salman", age: 22 },
   { id: 2, name: "Sami", age: 20 },
   { id: 3, name: "Sajeb", age: 21 },
 ];
+
+//connect to database
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://dbUser2:sGuOO1Bc0lYgje5M@cluster0.pea3c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("demo").collection("users");
+    // create a document to insert
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      user.id = users.length + 1;
+      users.push(user);
+      const result = await database.insertOne(user);
+      res.send(user);
+    });
+
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -30,12 +64,6 @@ app.get("/user/:id", (req, res) => {
   const id = req.params.id;
   const u = users[id];
   res.send(u);
-});
-app.post("/user", (req, res) => {
-  const user = req.body;
-  user.id = users.length + 1;
-  users.push(user);
-  res.send(user);
 });
 
 app.listen(port, () => {
